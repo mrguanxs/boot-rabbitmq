001664ec884c68322b2fa27230ed7515f00b67d7
guanxinshuai <guanxs@163.com>
2019-12-04 09:36:16 +0800

    init project and dependencies

---
+++ src/main/java/com/guan/bootrabbitmq/BootRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.guan.bootrabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootRabbitmqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guan/bootrabbitmq/BootRabbitmqApplicationTests.java
@@ -0,0 +1,14 @@
+package com.guan.bootrabbitmq;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootRabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
